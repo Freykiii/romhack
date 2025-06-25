f0e05e47cb90c3a7afe156b69b3249b7c9df49e3
Freykiii <frederic.claerhoudt@outlook.be>
2025-06-25 21:25:39 +0200

    running by default

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -829,8 +829,9 @@ static void PlayerNotOnBikeMoving(u8 direction, u16 heldKeys)
         return;
     }
 
-    if (!(gPlayerAvatar.flags & PLAYER_AVATAR_FLAG_UNDERWATER) && (heldKeys & B_BUTTON) && FlagGet(FLAG_SYS_B_DASH)
-     && IsRunningDisallowed(gObjectEvents[gPlayerAvatar.objectEventId].currentMetatileBehavior) == 0 && !FollowerNPCComingThroughDoor())
+    if (!(gPlayerAvatar.flags & PLAYER_AVATAR_FLAG_UNDERWATER) && !(heldKeys & B_BUTTON) 
+        && IsRunningDisallowed(gObjectEvents[gPlayerAvatar.objectEventId].currentMetatileBehavior) == 0 
+        && !FollowerNPCComingThroughDoor())
     {
         if (ObjectMovingOnRockStairs(&gObjectEvents[gPlayerAvatar.objectEventId], direction))
             PlayerRunSlow(direction);
@@ -852,6 +853,7 @@ static void PlayerNotOnBikeMoving(u8 direction, u16 heldKeys)
         else
             PlayerWalkNormal(direction);
     }
+
 }
 
 static u8 CheckForPlayerAvatarCollision(u8 direction)
